df7977b51039dc4bf9cac014578ac4cc8084723a
michelleheh <xiaoyu.he22@gmail.com>
2016-04-10 15:42:25 -0700

    Add spinner when loading

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -97,6 +97,9 @@ class Main extends React.Component{
       });
   }
   render() {
+    var showErr = (
+      this.state.error ? <Text> {this.state.error} </Text> : <View></View>
+    );
     return(
       <View style={styles.mainContainer}>
       <Text style={styles.title}> Search for a Github User </Text>
@@ -110,6 +113,12 @@ class Main extends React.Component{
           underlayColor="white">
           <Text style={styles.buttonText}> SEARCH </Text>
         </TouchableHighlight>
+        <ActivityIndicatorIOS
+          animating={this.state.isLoading}
+          color="#111"
+          size="large"
+        />
+        {showErr}
       </View>
       )
   }
