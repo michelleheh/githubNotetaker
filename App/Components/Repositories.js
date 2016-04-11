f75bb351eaf7cb5d0b59584b02232588323c1948
michelleheh <xiaoyu.he22@gmail.com>
2016-04-10 21:30:31 -0700

    Add getNotes and addNote api methos

--- App/Components/Repositories.js
+++ App/Components/Repositories.js
@@ -1,7 +1,7 @@
 var React = require('react-native');
 var Badge = require('./Badge');
 var Separator = require('./Separator');
-
+var Web_View = require('./WebView');
 var {
   ScrollView,
   Text,
@@ -37,7 +37,11 @@ var styles = StyleSheet.create({
 
 class Repositories extends React.Component{
   openPage(url){
-    console.log('the url is: ', url);
+    this.props.navigator.push({
+      component: Web_View,
+      title: 'Web View',
+      passProps: {url}
+    })
   }
   render(){
     var repos = this.props.repos;
--- App/Utils/api.js
+++ App/Utils/api.js
@@ -8,6 +8,19 @@ var api = {
     username = username.toLowerCase().trim();
     var url = `https://api.github.com/users/${username}/repos`;
     return fetch(url).then((res) => res.json());
+  },
+  getNoes(username){
+    username = username.toLowerCase().trim();
+    var url = `https://github-notetaker-rn.firebaseio.com/${username}.json`;
+    return fetch(url).then((res) => res.json());
+  },
+  addNote(username, note){
+    username = username.toLowerCase().trim();
+    var url = `https://github-notetaker-rn.firebaseio.com/${username}.json`;
+    return fetch(url, {
+      method: 'post',
+      body: JSON.stringigy(note)
+    }).then((res) => res.json());
   }
 }
 
